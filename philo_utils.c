421f373afd47e23cb3ca4ec5cab3e72f884e4cba
Demetrio Rodrigues <demrodri@c3r5s1.42wolfsburg.de>
2024-06-25 23:56:26 +0200

    structuring

--- philo_utils.c
+++ philo_utils.c
@@ -1,35 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
+/*   philo_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: demrodri <demrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/25 23:36:59 by demrodri          #+#    #+#             */
-/*   Updated: 2024/06/25 23:54:40 by demrodri         ###   ########.fr       */
+/*   Created: 2024/06/25 23:54:56 by demrodri          #+#    #+#             */
+/*   Updated: 2024/06/25 23:55:58 by demrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-#define N 5
+void    take_fork(int fork)
 
-void philosopher(int i)
-{
-    while (1)
-    {
-        think();//for some_time
-        take_fork(r_fork[i]);
-        
-        if (available(l_fork[i]))
-        {
-            take_fork(l_fork[i]);
-            eat();
-        }
-        
-        take_fork(philo_left[i]);
-        eat();
-        put_fork(philo_left[i]);
-        put_fork(philo_right[i]);        
-    }
-}
+void    eat()
+
+void    put_fork(int fork)
