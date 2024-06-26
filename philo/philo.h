036f90798aa9283cebb25e451f1ea477d14d3d55
Demetrio Rodrigues <demrodri@c3r5s2.42wolfsburg.de>
2024-06-26 15:14:52 +0200

    makefile and basic main

---
+++ philo/philo.h
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: demrodri <demrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 14:41:58 by demrodri          #+#    #+#             */
+/*   Updated: 2024/06/26 15:05:20 by demrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+void	philosopher(int i);
+
+#endif
+
+// # include "./libft/libft.h"
+// # include "./utils/color_code.h"
+// # include <assert.h>
+// # include <errno.h>
+// # include <fcntl.h>
+// # include <readline/history.h>
+// # include <readline/readline.h>
+// # include <signal.h>
+# include <stdio.h>
+// # include <stdlib.h>
+// # include <sys/wait.h>
+// # include <unistd.h>
+
+// # define MAX_CMD_LEN 1024
+
+// # define DEBUG 1
+
+// typedef struct s_main
+// {
+// 	char		*cmd;
+// 	char		**args;
+// 	int			fd[2];
+// 	int			syntaxflag;
+// 	pid_t		pid;
+// 	pid_t		grandson_pid;
+// }				t_main;
